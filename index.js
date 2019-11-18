e2bbe39fd0ced3299b2d43b0c08ee0acad7250d5
Nick <nick.moi@gmail.com>
2019-11-18 13:19:08 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+function writeCards(names, eventName){
+    let cards = [];
+    for(let i = 0; i < names.length; i++){
+        //console.log(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`)
+        cards.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`);
+    }
+    //console.log(cards)
+    return cards;
+}
+
+function countDown(number){
+    let countdown = number;
+    while (countdown > -1){
+        console.log(countdown);
+        countdown--;
+    }
+}
